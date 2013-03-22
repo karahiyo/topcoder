b5a4e0b39a7f7a72f6a82496795f4bab548dc919
karahiyo <a.ryuklnm@gmail.com>
2013-03-22 10:43:46 +0900

    full search.

--- InterestingDigits.java
+++ InterestingDigits.java
@@ -1,14 +1,31 @@
 import java.util.*;
 public class InterestingDigits {
     public int[] digits(int base) {
-        Vector<Integer> V = new Vector<Integer>();
-        for (int i=2;i<base; i++) {
-            if (((base-1)%i)==0)
-                V.add(i);
+        ArrayList<Integer> rec = new ArrayList<Integer>();
+
+        for(int n = 2; n<base; n++) {
+            boolean ok = true;
+            for(int k1=0; k1<base; k1++){
+                for(int k2=0; k2<base; k2++){
+                    for(int k3=0; k3<base; k3++) {
+                        if((k1+k2*base+k3*base*base)%n==0 &&
+                            (k1+k2+k3)%n != 0) {
+                            ok = false;
+                            break;
+                            }
+                        if(!ok) break;
+                    }
+                    if(!ok) break;
+                }
+                if(!ok) break;
+            }
+            if(ok) rec.add(n);
         }
-        int[] ans = new int[V.size()];
-        for(int i=0;i<V.size();i++)
-            ans[i] = V.get(i);
+
+        int ans[] = new int[rec.size()];
+        for(int i=0;i<rec.size();i++)
+            ans[i] = rec.get(i);
+
         return ans;
     }
 
