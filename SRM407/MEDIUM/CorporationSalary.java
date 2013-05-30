24b2beb13437f7d03e238e8ad5dec44887fcad90
karahiyo <a.ryuklnm@gmail.com>
2013-05-30 19:49:29 +0900

    SRM407 Div2 Medium

--- SRM407/MEDIUM/CorporationSalary.java
+++ SRM407/MEDIUM/CorporationSalary.java
@@ -1,141 +1,172 @@
 public class CorporationSalary {
-   public long totalSalary(String[] relations) {
-		
-   }
+    int[] memo;
+
+    public int calc(String[] relations, int p) {
+        if(memo[p] != 0)
+            return memo[p];
+
+        int sum = 0;
+        String arel = relations[p];
+        for(int i=0; i<arel.length(); i++) {
+            if(arel.charAt(i) == 'Y') {
+                if(memo[i] != 0) {
+                    sum += memo[i];
+                } else {
+                    memo[i] = calc(relations, i);
+                    sum += memo[i];
+                }
+            }
+        }
+        if(sum == 0)
+            return 1;
+        return sum;
+    }
+
+    public long totalSalary(String[] relations) {
+        int ret = 0;
+        int num = relations.length;
+        memo = new int[num];
+
+        for(int i=0;i<num;i++) {
+            ret += calc(relations, i);
+        }
+        return ret;
+
+    }
 
 
-// BEGIN CUT HERE
-   public static void main(String[] args) {
-		if (args.length == 0) {
-			CorporationSalaryHarness.run_test(-1);
-		} else {
-			for (int i=0; i<args.length; ++i)
-				CorporationSalaryHarness.run_test(Integer.valueOf(args[i]));
-		}
-	}
-// END CUT HERE
+    // BEGIN CUT HERE
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            CorporationSalaryHarness.run_test(-1);
+        } else {
+            for (int i=0; i<args.length; ++i)
+                CorporationSalaryHarness.run_test(Integer.valueOf(args[i]));
+        }
+    }
+    // END CUT HERE
 }
 
 // BEGIN CUT HERE
 class CorporationSalaryHarness {
-	public static void run_test(int casenum) {
-		if (casenum != -1) {
-			if (runTestCase(casenum) == -1)
-				System.err.println("Illegal input! Test case " + casenum + " does not exist.");
-			return;
-		}
-		
-		int correct = 0, total = 0;
-		for (int i=0;; ++i) {
-			int x = runTestCase(i);
-			if (x == -1) {
-				if (i >= 100) break;
-				continue;
-			}
-			correct += x;
-			++total;
-		}
-		
-		if (total == 0) {
-			System.err.println("No test cases run.");
-		} else if (correct < total) {
-			System.err.println("Some cases FAILED (passed " + correct + " of " + total + ").");
-		} else {
-			System.err.println("All " + total + " tests passed!");
-		}
-	}
-	
-	static boolean compareOutput(long expected, long result) { return expected == result; }
-	static String formatResult(long res) {
-		return String.format("%d", res);
-	}
-	
-	static int verifyCase(int casenum, long expected, long received) { 
-		System.err.print("Example " + casenum + "... ");
-		if (compareOutput(expected, received)) {
-			System.err.println("PASSED");
-			return 1;
-		} else {
-			System.err.println("FAILED");
-			System.err.println("    Expected: " + formatResult(expected)); 
-			System.err.println("    Received: " + formatResult(received)); 
-			return 0;
-		}
-	}
+    public static void run_test(int casenum) {
+        if (casenum != -1) {
+            if (runTestCase(casenum) == -1)
+                System.err.println("Illegal input! Test case " + casenum + " does not exist.");
+            return;
+        }
 
-	static int runTestCase(int casenum__) {
-		switch(casenum__) {
-		case 0: {
-			String[] relations        = {"N"};
-			long expected__           = 1;
+        int correct = 0, total = 0;
+        for (int i=0;; ++i) {
+            int x = runTestCase(i);
+            if (x == -1) {
+                if (i >= 100) break;
+                continue;
+            }
+            correct += x;
+            ++total;
+        }
 
-			return verifyCase(casenum__, expected__, new CorporationSalary().totalSalary(relations));
-		}
-		case 1: {
-			String[] relations        = {"NNYN",
- "NNYN",
- "NNNN",
- "NYYN"};
-			long expected__           = 5;
+        if (total == 0) {
+            System.err.println("No test cases run.");
+        } else if (correct < total) {
+            System.err.println("Some cases FAILED (passed " + correct + " of " + total + ").");
+        } else {
+            System.err.println("All " + total + " tests passed!");
+        }
+    }
 
-			return verifyCase(casenum__, expected__, new CorporationSalary().totalSalary(relations));
-		}
-		case 2: {
-			String[] relations        = {"NNNNNN",
- "YNYNNY",
- "YNNNNY",
- "NNNNNN",
- "YNYNNN",
- "YNNYNN"};
-			long expected__           = 17;
+    static boolean compareOutput(long expected, long result) { return expected == result; }
+    static String formatResult(long res) {
+        return String.format("%d", res);
+    }
 
-			return verifyCase(casenum__, expected__, new CorporationSalary().totalSalary(relations));
-		}
-		case 3: {
-			String[] relations        = {"NYNNYN",
- "NNNNNN",
- "NNNNNN",
- "NNYNNN",
- "NNNNNN",
- "NNNYYN"};
-			long expected__           = 8;
+    static int verifyCase(int casenum, long expected, long received) { 
+        System.err.print("Example " + casenum + "... ");
+        if (compareOutput(expected, received)) {
+            System.err.println("PASSED");
+            return 1;
+        } else {
+            System.err.println("FAILED");
+            System.err.println("    Expected: " + formatResult(expected)); 
+            System.err.println("    Received: " + formatResult(received)); 
+            return 0;
+        }
+    }
 
-			return verifyCase(casenum__, expected__, new CorporationSalary().totalSalary(relations));
-		}
-		case 4: {
-			String[] relations        = {"NNNN",
- "NNNN",
- "NNNN",
- "NNNN"};
-			long expected__           = 4;
+    static int runTestCase(int casenum__) {
+        switch(casenum__) {
+            case 0: {
+                        String[] relations        = {"N"};
+                        long expected__           = 1;
 
-			return verifyCase(casenum__, expected__, new CorporationSalary().totalSalary(relations));
-		}
+                        return verifyCase(casenum__, expected__, new CorporationSalary().totalSalary(relations));
+            }
+            case 1: {
+                        String[] relations        = {"NNYN",
+                            "NNYN",
+                            "NNNN",
+                            "NYYN"};
+                        long expected__           = 5;
 
-		// custom cases
+                        return verifyCase(casenum__, expected__, new CorporationSalary().totalSalary(relations));
+            }
+            case 2: {
+                        String[] relations        = {"NNNNNN",
+                            "YNYNNY",
+                            "YNNNNY",
+                            "NNNNNN",
+                            "YNYNNN",
+                            "YNNYNN"};
+                        long expected__           = 17;
 
-/*      case 5: {
-			String[] relations        = ;
-			long expected__           = ;
+                        return verifyCase(casenum__, expected__, new CorporationSalary().totalSalary(relations));
+            }
+            case 3: {
+                        String[] relations        = {"NYNNYN",
+                            "NNNNNN",
+                            "NNNNNN",
+                            "NNYNNN",
+                            "NNNNNN",
+                            "NNNYYN"};
+                        long expected__           = 8;
 
-			return verifyCase(casenum__, expected__, new CorporationSalary().totalSalary(relations));
-		}*/
-/*      case 6: {
-			String[] relations        = ;
-			long expected__           = ;
+                        return verifyCase(casenum__, expected__, new CorporationSalary().totalSalary(relations));
+            }
+            case 4: {
+                        String[] relations        = {"NNNN",
+                            "NNNN",
+                            "NNNN",
+                            "NNNN"};
+                        long expected__           = 4;
 
-			return verifyCase(casenum__, expected__, new CorporationSalary().totalSalary(relations));
-		}*/
-/*      case 7: {
-			String[] relations        = ;
-			long expected__           = ;
+                        return verifyCase(casenum__, expected__, new CorporationSalary().totalSalary(relations));
+            }
 
-			return verifyCase(casenum__, expected__, new CorporationSalary().totalSalary(relations));
-		}*/
-		default:
-			return -1;
-		}
-	}
+            // custom cases
+
+            /*      case 5: {
+                    String[] relations        = ;
+                    long expected__           = ;
+
+                    return verifyCase(casenum__, expected__, new CorporationSalary().totalSalary(relations));
+                    }*/
+            /*      case 6: {
+                    String[] relations        = ;
+                    long expected__           = ;
+
+                    return verifyCase(casenum__, expected__, new CorporationSalary().totalSalary(relations));
+                    }*/
+            /*      case 7: {
+                    String[] relations        = ;
+                    long expected__           = ;
+
+                    return verifyCase(casenum__, expected__, new CorporationSalary().totalSalary(relations));
+                    }*/
+            default:
+                    return -1;
+        }
+    }
 }
 
 // END CUT HERE
