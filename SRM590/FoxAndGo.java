2473c299fd6e9510538758d8a8e69b7761a7be0f
karahiyo <a.ryuklnm@gmail.com>
2013-09-08 16:30:42 +0900

    SRM590 Medium

--- SRM590/FoxAndGo.java
+++ SRM590/FoxAndGo.java
@@ -1,28 +1,87 @@
 import java.util.*;
+import java.math.*;
 
 public class FoxAndGo {
-    public int maxKill(String[] b) {
 
-        int max = 0;
+    private static String[] board;
+
+    public static int maxKill(String[] b) {
+
+        int ret = 0;
         int h = b.length;
         int w = b[0].length();
 
-        int[][] checked = new int[h][w];
-
         for ( int i=0; i<h; i++) {
-            for( int j = 0; j<w; j++) {
-                if(b[i].charAt(j) == 'o') {
-                    Queue<Integer
-                    int[] x = (i, j + 1);
-                    int[] y = 
+            for( int j=0; j<w; j++) {
 
-                
+                if(b[i].charAt(j) != '.')
+                    continue;
+
+                FoxAndGo.board = Arrays.copyOf(b, b.length);
+                FoxAndGo.board[i] = FoxAndGo.board[i].substring(0, j) 
+                    + 'x' 
+                    + FoxAndGo.board[i].substring(j+1);
+
+                int max = 0;
+                for(int x=0; x<h; x++) {
+                    for(int y=0; y<w; y++) {
+                        if(FoxAndGo.board[x].charAt(y) == 'o'
+                                && isDead(x, y)) {
+                            max += getDeadGroup(x, y);
+                                }
+                    }
                 }
+
+                ret = Math.max(ret, max);
             }
         }
-        
-
+        return ret;
     }
+
+    public static boolean isDead(int x, int y) {
+        if( x < 0 || y < 0 || 
+            x >= board.length || y >= board[0].length())
+            return true;
+
+        if(board[x].charAt(y) == 'x' || board[x].charAt(y) == '-')
+            return true;
+        else if(board[x].charAt(y) == '.')
+            return false;
+
+
+        if (board[x].charAt(y) == 'o')
+            board[x] = board[x].substring(0, y) + '-' + board[x].substring(y+1);
+
+        if(!isDead(x, y+1))
+            return false;
+        if(!isDead(x+1, y))
+            return false;
+        if(!isDead(x, y-1))
+            return false;
+        if(!isDead(x-1, y))
+            return false;
+
+        return true;
+    }
+
+    public static int getDeadGroup(int x, int y) {
+        int N = 0;
+        if(x < 0 || y < 0 ||
+            x >= board.length || y >= board[0].length())
+            return N;
+
+        if(board[x].charAt(y) != '-')
+            return N;
+
+        board[x] = board[x].substring(0, y) + '*' + board[x].substring(y+1);
+        N = 1;
+        return N + getDeadGroup(x+1, y) 
+                 + getDeadGroup(x, y+1)
+                 + getDeadGroup(x-1, y)
+                 + getDeadGroup(x, y-1);
+    }
+
+
 
 
     // BEGIN CUT HERE
