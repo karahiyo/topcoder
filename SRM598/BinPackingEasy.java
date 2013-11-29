77259c045aee23fe6bf66ed5e14073cd7dbd7380
karahiyo <a.ryuklnm@gmail.com>
2013-11-29 15:06:20 +0900

    SRM598

---
+++ SRM598/BinPackingEasy.java
@@ -0,0 +1,157 @@
+import java.util.*;
+
+public class BinPackingEasy {
+    public int minBins(int[] item) {
+        int ret = 0;
+        List<Integer> is = new ArrayList<Integer>();
+        for ( int i=0; i<item.length; i++) {
+            is.add(item[i]);
+        }
+
+        while(true) {
+            if (is.isEmpty()) {
+                break;
+            }
+            if (is.size() <= 1) {
+                ret++;
+                break;
+            }
+            int base = is.get(0);
+            is.remove(0);
+            int max = base;
+            int p = 0;
+            for ( int i=0; i<is.size(); i++) {
+                if (max < base + is.get(i) && base + is.get(i) <= 300) {
+                    max = base + is.get(i);
+                    p = i;
+                }
+            }
+            ret++;
+            if ( max != base) {
+                is.remove(p);
+            }
+        }
+        return ret;
+
+    }
+
+
+    // BEGIN CUT HERE
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            BinPackingEasyHarness.run_test(-1);
+        } else {
+            for (int i=0; i<args.length; ++i)
+                BinPackingEasyHarness.run_test(Integer.valueOf(args[i]));
+        }
+    }
+    // END CUT HERE
+}
+
+// BEGIN CUT HERE
+class BinPackingEasyHarness {
+    public static void run_test(int casenum) {
+        if (casenum != -1) {
+            if (runTestCase(casenum) == -1)
+                System.err.println("Illegal input! Test case " + casenum + " does not exist.");
+            return;
+        }
+
+        int correct = 0, total = 0;
+        for (int i=0;; ++i) {
+            int x = runTestCase(i);
+            if (x == -1) {
+                if (i >= 100) break;
+                continue;
+            }
+            correct += x;
+            ++total;
+        }
+
+        if (total == 0) {
+            System.err.println("No test cases run.");
+        } else if (correct < total) {
+            System.err.println("Some cases FAILED (passed " + correct + " of " + total + ").");
+        } else {
+            System.err.println("All " + total + " tests passed!");
+        }
+    }
+
+    static boolean compareOutput(int expected, int result) { return expected == result; }
+    static String formatResult(int res) {
+        return String.format("%d", res);
+    }
+
+    static int verifyCase(int casenum, int expected, int received) { 
+        System.err.print("Example " + casenum + "... ");
+        if (compareOutput(expected, received)) {
+            System.err.println("PASSED");
+            return 1;
+        } else {
+            System.err.println("FAILED");
+            System.err.println("    Expected: " + formatResult(expected)); 
+            System.err.println("    Received: " + formatResult(received)); 
+            return 0;
+        }
+    }
+
+    static int runTestCase(int casenum__) {
+        switch(casenum__) {
+            case 0: {
+                        int[] item                = {150, 150, 150, 150, 150};
+                        int expected__            = 3;
+
+                        return verifyCase(casenum__, expected__, new BinPackingEasy().minBins(item));
+            }
+            case 1: {
+                        int[] item                = {130, 140, 150, 160};
+                        int expected__            = 2;
+
+                        return verifyCase(casenum__, expected__, new BinPackingEasy().minBins(item));
+            }
+            case 2: {
+                        int[] item                = {101, 101, 101, 101, 101, 101, 101, 101, 101};
+                        int expected__            = 5;
+
+                        return verifyCase(casenum__, expected__, new BinPackingEasy().minBins(item));
+            }
+            case 3: {
+                        int[] item                = {101, 200, 101, 101, 101, 101, 200, 101, 200};
+                        int expected__            = 6;
+
+                        return verifyCase(casenum__, expected__, new BinPackingEasy().minBins(item));
+            }
+            case 4: {
+                        int[] item                = {123, 145, 167, 213, 245, 267, 289, 132, 154, 176, 198};
+                        int expected__            = 8;
+
+                        return verifyCase(casenum__, expected__, new BinPackingEasy().minBins(item));
+            }
+
+            // custom cases
+
+            /*      case 5: {
+                    int[] item                = ;
+                    int expected__            = ;
+
+                    return verifyCase(casenum__, expected__, new BinPackingEasy().minBins(item));
+                    }*/
+            /*      case 6: {
+                    int[] item                = ;
+                    int expected__            = ;
+
+                    return verifyCase(casenum__, expected__, new BinPackingEasy().minBins(item));
+                    }*/
+            /*      case 7: {
+                    int[] item                = ;
+                    int expected__            = ;
+
+                    return verifyCase(casenum__, expected__, new BinPackingEasy().minBins(item));
+                    }*/
+            default:
+                    return -1;
+        }
+    }
+}
+
+// END CUT HERE
---
+++ SRM598/ErasingCharacters.java
@@ -0,0 +1,142 @@
+
+public class ErasingCharacters {
+    public String simulate(String s) {
+        while(true) {
+            String t_s = s;
+            for(int i=0; i < s.length()-1; i++){
+                if (s.charAt(i) == s.charAt(i+1)) {
+                    StringBuilder sb = new StringBuilder(s);
+                    sb.deleteCharAt(i);
+                    sb.deleteCharAt(i);
+                    s = sb.toString();
+                    break;
+                }
+            }
+            if ( s.equals(t_s)) {
+                break;
+            }
+        }
+        return s;
+
+    }
+
+
+    // BEGIN CUT HERE
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            ErasingCharactersHarness.run_test(-1);
+        } else {
+            for (int i=0; i<args.length; ++i)
+                ErasingCharactersHarness.run_test(Integer.valueOf(args[i]));
+        }
+    }
+    // END CUT HERE
+}
+
+// BEGIN CUT HERE
+class ErasingCharactersHarness {
+    public static void run_test(int casenum) {
+        if (casenum != -1) {
+            if (runTestCase(casenum) == -1)
+                System.err.println("Illegal input! Test case " + casenum + " does not exist.");
+            return;
+        }
+
+        int correct = 0, total = 0;
+        for (int i=0;; ++i) {
+            int x = runTestCase(i);
+            if (x == -1) {
+                if (i >= 100) break;
+                continue;
+            }
+            correct += x;
+            ++total;
+        }
+
+        if (total == 0) {
+            System.err.println("No test cases run.");
+        } else if (correct < total) {
+            System.err.println("Some cases FAILED (passed " + correct + " of " + total + ").");
+        } else {
+            System.err.println("All " + total + " tests passed!");
+        }
+    }
+
+    static boolean compareOutput(String expected, String result) { return expected.equals(result); }
+    static String formatResult(String res) {
+        return String.format("\"%s\"", res);
+    }
+
+    static int verifyCase(int casenum, String expected, String received) { 
+        System.err.print("Example " + casenum + "... ");
+        if (compareOutput(expected, received)) {
+            System.err.println("PASSED");
+            return 1;
+        } else {
+            System.err.println("FAILED");
+            System.err.println("    Expected: " + formatResult(expected)); 
+            System.err.println("    Received: " + formatResult(received)); 
+            return 0;
+        }
+    }
+
+    static int runTestCase(int casenum__) {
+        switch(casenum__) {
+            case 0: {
+                        String s                  = "cieeilll";
+                        String expected__         = "cl";
+
+                        return verifyCase(casenum__, expected__, new ErasingCharacters().simulate(s));
+            }
+            case 1: {
+                        String s                  = "topcoder";
+                        String expected__         = "topcoder";
+
+                        return verifyCase(casenum__, expected__, new ErasingCharacters().simulate(s));
+            }
+            case 2: {
+                        String s                  = "abcdefghijklmnopqrstuvwxyyxwvutsrqponmlkjihgfedcba";
+                        String expected__         = "";
+
+                        return verifyCase(casenum__, expected__, new ErasingCharacters().simulate(s));
+            }
+            case 3: {
+                        String s                  = "bacaabaccbaaccabbcabbacabcbba";
+                        String expected__         = "bacbaca";
+
+                        return verifyCase(casenum__, expected__, new ErasingCharacters().simulate(s));
+            }
+            case 4: {
+                        String s                  = "eel";
+                        String expected__         = "l";
+
+                        return verifyCase(casenum__, expected__, new ErasingCharacters().simulate(s));
+            }
+
+            // custom cases
+
+            /*      case 5: {
+                    String s                  = ;
+                    String expected__         = ;
+
+                    return verifyCase(casenum__, expected__, new ErasingCharacters().simulate(s));
+                    }*/
+            /*      case 6: {
+                    String s                  = ;
+                    String expected__         = ;
+
+                    return verifyCase(casenum__, expected__, new ErasingCharacters().simulate(s));
+                    }*/
+            /*      case 7: {
+                    String s                  = ;
+                    String expected__         = ;
+
+                    return verifyCase(casenum__, expected__, new ErasingCharacters().simulate(s));
+                    }*/
+            default:
+                    return -1;
+        }
+    }
+}
+
+// END CUT HERE
